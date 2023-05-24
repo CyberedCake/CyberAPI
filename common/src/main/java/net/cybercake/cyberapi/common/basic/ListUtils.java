8894a83714ad1fc01c1b003d00020acb3d4de982
CyberedCake <61170080+CyberedCake@users.noreply.github.com>
2023-05-23 21:11:16 -0400

    Add two ListUtils#addToList methods for List modifications in one line

--- common/src/main/java/net/cybercake/cyberapi/common/basic/ListUtils.java
+++ common/src/main/java/net/cybercake/cyberapi/common/basic/ListUtils.java
@@ -1,5 +1,8 @@
 package net.cybercake.cyberapi.common.basic;
 
+import com.google.common.base.Preconditions;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,14 +31,46 @@ public class ListUtils {
      * @return the list without duplicates
      * @since 102
      */
-    public static ArrayList<String> removeDuplicates(ArrayList<String> list) {
-        ArrayList<String> alreadyOver = new ArrayList<>();
+    public static List<String> removeDuplicates(ArrayList<String> list) {
+        List<String> alreadyOver = new ArrayList<>();
         for(String str : list) {
             if(!alreadyOver.contains(str)) {
                 alreadyOver.add(str);
             }
         }
         return alreadyOver;
+    }
+
+    /**
+     * Add an item to a list in one line of code
+     * @param originalList the original list that needs to have an item added to it
+     * @param addition the item that needs to be added to the {@code originalList}
+     * @return the new list with the {@code addition} appended onto the end
+     * @param <T> the type of list
+     * @since 127
+     * @see ListUtils#addToList(List, Object, List) add item to a list with a default list
+     */
+    public static <T> List<T> addToList(List<T> originalList, T addition) {
+        return addToList(originalList, addition, null);
+    }
+
+    /**
+     * Add an item to a list (if it exists) in one line of code
+     * @param originalList the original list that needs to have an item added to it
+     * @param addition the item that needs to be added to the {@code originalList} or the {@code defaultList} if the {@code originalList} is not set (or null)
+     * @param def the default list which the item will be added to if the originalList is null -- if this item is null, the added item will create it's own list
+     * @return the new list with the {@code addition} appended onto the end
+     * @param <T> the type of list
+     * @since 127
+     * @see ListUtils#addToList(List, Object) same method with the default list automatically set to null
+     */
+    public static <T> List<T> addToList(List<T> originalList, T addition, @Nullable List<T> def) {
+        if(originalList == null || originalList.size() < 1) {
+            Preconditions.checkNotNull(def, "The default list is set to null, cannot add to a non-existent list!");
+            originalList = def;
+        }
+        originalList.add(addition);
+        return originalList;
     }
 
 }
