fa97de68741ea4fc21fdd57a308541f3eaa41cfc
CyberedCake <oerooero1254@gmail.com>
2023-07-18 19:01:00 -0400

    Add more natural slot methods

--- spigot/src/main/java/net/cybercake/cyberapi/spigot/inventory/pagination/NaturallyPaginatedGUI.java
+++ spigot/src/main/java/net/cybercake/cyberapi/spigot/inventory/pagination/NaturallyPaginatedGUI.java
@@ -4,13 +4,16 @@ import net.cybercake.cyberapi.spigot.inventory.CustomGUI;
 import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
+import org.checkerframework.common.value.qual.ArrayLen;
+import org.checkerframework.common.value.qual.ArrayLenRange;
+import org.checkerframework.common.value.qual.IntRange;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Function;
 
 public abstract class NaturallyPaginatedGUI extends CustomGUI {
 
-    protected NaturallyPaginatedGUI(Function<NaturallyPaginatedGUI, Inventory> createInventory, int[] naturalSlots) {
+    protected NaturallyPaginatedGUI(Function<NaturallyPaginatedGUI, Inventory> createInventory) {
         super((gui) -> createInventory.apply((NaturallyPaginatedGUI) gui));
     }
 
@@ -30,5 +33,22 @@ public abstract class NaturallyPaginatedGUI extends CustomGUI {
         this((gui) -> Bukkit.createInventory(gui, type, type.getDefaultTitle()));
     }
 
+    private int[] naturalSlots;
+
+    public void setNaturalSlots(int[] naturalSlots) {
+        this.naturalSlots = naturalSlots;
+    }
+
+    public int[] getNaturalSlots() { return this.naturalSlots; }
+
+    public void addNewNaturalSlot(int newSlot) {
+        int[] newNaturalSlots = new int[this.naturalSlots.length+1];
+        newNaturalSlots[this.naturalSlots.length] = newSlot;
+        this.naturalSlots = newNaturalSlots;
+    }
+
+    public @ArrayLenRange(from = 1, to = 1) int[] naturalSlot() {
+        return this.naturalSlots;
+    }
 
 }
